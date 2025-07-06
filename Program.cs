82e20cf0aefaa50051f62937e702b862a1994193
GuptaHeer27 <guptaheer27@gmail.com>
2025-07-06 23:12:05 +0530

    Add files via upload
    LoginRequest.cs
    Program.cs
    User.cs
    RegisterRequest.cs
    appsettings.Development.json
    WeatherForecast.cs
    WeatherForecastController.cs files are added in Day 3

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using LoginAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// PostgreSQL connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
